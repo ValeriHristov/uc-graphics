3e5ac044cf7fd4b7bfcbb8911742fe52151623dc
Stefan Dyulgerov <stefan.dyulgerov@gmail.com>
2019-03-07 08:16:05 +0200

    moved functions

--- src/src/uc/gx/dx12/cmd/command_queue.cpp
+++ src/src/uc/gx/dx12/cmd/command_queue.cpp
@@ -9,6 +9,16 @@ namespace uc
     {
         namespace dx12
         {
+            void gpu_command_queue::signal_fence(ID3D12Fence* f, uint64_t value) 
+            {
+                throw_if_failed(m_queue->Signal(f, value));
+            }
+
+            void gpu_command_queue::signal_fence(uint64_t value) 
+            {
+                throw_if_failed(m_queue->Signal(m_fence.Get(), value));
+            }
+
             void gpu_command_queue::pix_begin_event(const wchar_t* label)
             {
                 PIXBeginEvent(m_queue.Get(), 0, label);
