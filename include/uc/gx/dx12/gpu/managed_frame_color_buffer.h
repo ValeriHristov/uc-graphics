3ce6b665187c40c89be797ab6819e33100c83b2e
kingofthebongo2008 <stefan.dyulgerov@gmail.com>
2019-04-27 18:53:52 +0300

    fix a bug

--- include/uc/gx/dx12/gpu/managed_frame_color_buffer.h
+++ include/uc/gx/dx12/gpu/managed_frame_color_buffer.h
@@ -25,7 +25,7 @@ namespace uc
             using managed_gpu_frame_color_buffer = std::unique_ptr< gpu_frame_color_buffer, details::gpu_frame_color_buffer_deleter >;
 
             template <typename ...args>
-            inline gpu_frame_color_buffer create_frame_color_buffer(gpu_resource_create_context* rc, args&&... a)
+            inline managed_gpu_frame_color_buffer create_frame_color_buffer(gpu_resource_create_context* rc, args&&... a)
             {
                 return managed_gpu_frame_color_buffer(rc->create_frame_color_buffer(std::forward<args>(a)...), details::gpu_frame_color_buffer_deleter(rc));
             }
